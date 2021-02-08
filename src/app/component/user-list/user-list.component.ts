e9b78e747a52b839ab10db52a7321ac4dfdb4a13
BELÉN PULIDO RIVERO <belen.pulido@ikeasi.com>
2021-02-08 14:05:36 +0000

    reload userlist

--- src/app/component/user-list/user-list.component.ts
+++ src/app/component/user-list/user-list.component.ts
@@ -16,6 +16,7 @@ export class UserListComponent implements OnInit {
 
   ngOnInit(): void {
     this.userService.getAllUsers().subscribe(data=>this.usuarios=data);
+    this.reloadPage();
   }
 
   editUser(nombreusuario){
@@ -32,4 +33,19 @@ export class UserListComponent implements OnInit {
       }
     })
   }
+
+  reloadPage() {
+    // The last "domLoading" Time //
+    var currentDocumentTimestamp =
+    new Date(performance.timing.domLoading).getTime();
+    // Current Time //
+    var now = Date.now();
+    // Ten Seconds //
+    var tenSec = 10 * 1000;
+    // Plus Ten Seconds //
+    var plusTenSec = currentDocumentTimestamp + tenSec;
+    if (now > plusTenSec) {
+    window.location.reload();
+    } else {}
+    }
 }
