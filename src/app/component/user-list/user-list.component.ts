d386a635f32544bf7e7f3759ee17899855f5cef9
BELÉN PULIDO RIVERO <belen.pulido@ikeasi.com>
2021-02-09 15:26:57 +0000

    pagination of user list for admin

--- src/app/component/user-list/user-list.component.ts
+++ src/app/component/user-list/user-list.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Usuario } from 'src/app/model/UsuarioModel';
 
 @Component({
   selector: 'app-user-list',
@@ -12,10 +16,20 @@ import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component'
 export class UserListComponent implements OnInit {
   usuarios;
   add_url;
+  columnas: string[] = ['nombreusuario', 'rol', 'nombre', 'apellidos','ciudad','telefono', 'acciones'];
+
+  @ViewChild(MatPaginator, { static: true }) paginator1: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource=null;
   constructor(private userService: UserService, private router: Router, public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.userService.getAllUsers().subscribe(data=>this.usuarios=data);
+    this.userService.getAllUsers().subscribe(data=>{
+      this.usuarios=data
+      this.dataSource = new MatTableDataSource<Usuario>(this.usuarios);
+        this.dataSource.sort=this.sort;
+        this.dataSource.paginator = this.paginator1;
+    });
     this.reloadPage();
   }
 
